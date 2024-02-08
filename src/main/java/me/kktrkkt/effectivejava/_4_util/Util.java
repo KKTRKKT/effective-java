76725de44e4d8c866bee78c3005547ac07f84294
shlee <paulhana6006@gmail.com>
2024-02-08 12:01:54 +0900

    Util 설명 추가

---
+++ src/main/java/me/kktrkkt/effectivejava/_4_util/Util.java
@@ -0,0 +1,9 @@
+package me.kktrkkt.effectivejava._4_util;
+
+// private 메소드로 생성을 막고 리플렉션까지 막기 위해서 에러를 던진다.
+public class Util {
+
+    private Util(){
+        throw new AssertionError();
+    }
+}
---
+++ src/main/java/me/kktrkkt/effectivejava/_4_util/package-info.java
@@ -0,0 +1,6 @@
+package me.kktrkkt.effectivejava._4_util;
+
+/*
+인스턴스화를 막기 위해서 private 생성자를 사용한다.
+    유틸리티 클래스는 인스턴스가 필요없이 정적메소드로만 구성되어 있으므로 생성자를 막아야한다
+ */
