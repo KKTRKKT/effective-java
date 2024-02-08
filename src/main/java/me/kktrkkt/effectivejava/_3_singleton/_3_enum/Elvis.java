908f82b0b6c6932ea19d100aaade830a48991864
shlee <paulhana6006@gmail.com>
2024-02-08 10:10:53 +0900

    enum 싱글톤 설명 추가

---
+++ src/main/java/me/kktrkkt/effectivejava/_3_singleton/_3_enum/Elvis.java
@@ -0,0 +1,18 @@
+package me.kktrkkt.effectivejava._3_singleton._3_enum;
+
+import me.kktrkkt.effectivejava._3_singleton._2_private_static_final.Concert;
+import me.kktrkkt.effectivejava._3_singleton._2_private_static_final.Singer;
+
+public enum Elvis implements Singer {
+    INSTANCE;
+
+    @Override
+    public void sing() {
+        System.out.println("I'll have a blue- Christmas without you~");
+    }
+
+    public static void main(String[] args) {
+        Concert concert = new Concert();
+        concert.start(()->INSTANCE);
+    }
+}
