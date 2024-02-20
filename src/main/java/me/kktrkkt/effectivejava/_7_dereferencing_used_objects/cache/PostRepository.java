977829c75e047e07d330d9cb3e8e2ae9c3540461
shlee <paulhana6006@gmail.com>
2024-02-20 17:12:51 +0900

    WeakHashMap 주의점 추가

--- src/main/java/me/kktrkkt/effectivejava/_7_dereferencing_used_objects/cache/PostRepository.java
+++ src/main/java/me/kktrkkt/effectivejava/_7_dereferencing_used_objects/cache/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 // Weak 레퍼런스를 이용한 자료구조를 사용하면 객체 사용이 끝나는 시점에 GC에 의해 수거가 가능하다.
+// 주의할 점은 primitive 타입을 키로 쓰면 jvm 내부에서 캐싱되어서 GC에 수거가 되지 않는다.
 public class PostRepository {
 
     Map<CacheKey, Post> cache;
