a7d88193694fb90b26fb62c28c2648fde39f99a1
KKTRKKT <paulhana6006@gmail.com>
2024-02-21 22:37:12 +0900

    동시성 프로그래밍 설명 추가

---
+++ src/main/java/me/kktrkkt/effectivejava/etc/concurrency/package-info.java
@@ -0,0 +1,15 @@
+package me.kktrkkt.effectivejava.etc.concurrency;
+
+/*
+
+Thread 아래 링크 참고
+https://github.com/KKTRKKT/java8to11/tree/master/src/main/java/me/kktrkkt/java8to11/concurrent
+https://kktrkkt.github.io/java/2023/01/30/Concurrent-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D/
+
+Executor 아래 링크 참고
+https://github.com/KKTRKKT/java8to11/blob/master/src/main/java/me/kktrkkt/java8to11/executors/App.java
+https://kktrkkt.github.io/java/2023/02/02/Executors-1/
+https://kktrkkt.github.io/java/2023/02/03/Executors-2/
+https://kktrkkt.github.io/java/2023/02/04/CompletableFuture-1/
+https://kktrkkt.github.io/java/2023/02/04/CompletableFuture-2/
+ */
