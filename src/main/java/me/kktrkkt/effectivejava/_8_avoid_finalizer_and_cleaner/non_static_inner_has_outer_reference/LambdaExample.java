ad7005e2c584df21f41a5b66c64d0c93478a8edb
KKTRKKT <paulhana6006@gmail.com>
2024-02-25 14:32:01 +0900

    정적이 아닌 내부 객체는 바깥 객체 참조 및 람다 바깥 객체 참조 설명 및 예제 추가

---
+++ src/main/java/me/kktrkkt/effectivejava/_8_avoid_finalizer_and_cleaner/non_static_inner_has_outer_reference/LambdaExample.java
@@ -0,0 +1,26 @@
+package me.kktrkkt.effectivejava._8_avoid_finalizer_and_cleaner.non_static_inner_has_outer_reference;
+
+import java.util.Arrays;
+
+// 람다에서 외부 객체의 프로퍼티를 참조하는 경우 외부 객체 참조가 생긴다.
+// 하지만 value가 static인 경우에는 외부 객체 참조가 생기지 않는다.
+public class LambdaExample {
+
+    private int value = 1;
+
+    private Runnable lamda = () -> System.out.println(value);
+
+    public static void main(String[] args) {
+        final LambdaExample lambdaExample = new LambdaExample();
+        printAllField(lambdaExample.lamda);
+    }
+
+    public static void printAllField(Object obj) {
+        final Class<?> aClass = obj.getClass();
+        Arrays.stream(aClass.getDeclaredFields()).forEach(f->{
+            System.out.println("type : " + f.getType().getSimpleName());
+            System.out.println("name : " + f.getName());
+        });
+    }
+
+}
---
+++ src/main/java/me/kktrkkt/effectivejava/_8_avoid_finalizer_and_cleaner/non_static_inner_has_outer_reference/OuterClass.java
@@ -0,0 +1,23 @@
+package me.kktrkkt.effectivejava._8_avoid_finalizer_and_cleaner.non_static_inner_has_outer_reference;
+
+import java.util.Arrays;
+
+public class OuterClass {
+
+    class InnerClass {
+    }
+
+    public static void main(String[] args) {
+        final OuterClass outerClass = new OuterClass();
+        final InnerClass innerClass = outerClass.new InnerClass();
+        printAllField(innerClass);
+    }
+
+    public static void printAllField(Object obj) {
+        final Class<?> aClass = obj.getClass();
+        Arrays.stream(aClass.getDeclaredFields()).forEach(f->{
+            System.out.println("type : " + f.getType().getSimpleName());
+            System.out.println("name : " + f.getName());
+        });
+    }
+}
