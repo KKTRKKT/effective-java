b37357f93df169d79a6b3c6886a3aafbde2f0f3b
shlee <paulhana6006@gmail.com>
2024-03-26 18:31:28 +0900

    public 클래스는 접근 메소드 사용 설명 추가

---
+++ src/main/java/me/kktrkkt/effectivejava/_16_public_class_use_access_method/package-info.java
@@ -0,0 +1,10 @@
+package me.kktrkkt.effectivejava._16_public_class_use_access_method;
+
+/*
+public 클래스는 public 필드가 아닌 접근 메소드를 사용하라
+    public 필드를 사용할 경우 문제점
+    - 캡슐화의 장점을 가지지 못한다.
+    - 필드가 변경될때 API도 변경되어야 한다
+    - 필드를 설정할때 부수 작업을 할 수 없다
+    단, private 클래스일 경우에는 public 필드를 사용해도 괜찮다
+ */
